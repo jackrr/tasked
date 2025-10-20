d0f0c1cb36ea95f48cc8177b67ff241cecf59f63
Jack Ratner <jackratner@gmail.com>
2025-10-20 09:22:09 -0400

    ha a little less over-engineering
    Take advantage of in-memory sqlite for tests, instead of my
    heavy-handed file management solution.

--- server/src/test_helpers.rs
+++ server/src/test_helpers.rs
@@ -2,8 +2,6 @@ use super::initialize_rocket;
 use migration::{Migrator, MigratorTrait};
 use rocket::local::asynchronous::Client;
 use sea_orm::{Database, DatabaseConnection};
-use std::time::SystemTime;
-use uuid::Uuid;
 
 pub async fn init_server(db: Option<DatabaseConnection>) -> anyhow::Result<Client> {
     let conn = match db {
@@ -23,15 +21,7 @@ pub async fn init_server(db: Option<DatabaseConnection>) -> anyhow::Result<Clien
 }
 
 pub async fn db_conn() -> anyhow::Result<DatabaseConnection> {
-    // TODO: establish "teardown" pattern to remove temporary db file
-    let now = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)?
-        .as_millis();
-    let nonce = Uuid::new_v4();
-    let db_uri = format!("sqlite://../tmp/test_db_{now}_{nonce}.sqlite?mode=rwc");
-    // Log DB URI, can be helpful for debugging failed tests
-    println!("DB URI {db_uri}");
-    let conn = Database::connect(db_uri).await?;
+    let conn = Database::connect("sqlite::memory:").await?;
     Migrator::refresh(&conn).await?;
     Ok(conn)
 }
